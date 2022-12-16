4696f987d52c13b2c70566e2cf3e5e4d1dc86993
AndrewJohnValdez <andrewjohnv@cpp.edu>
2022-12-15 23:46:16 -0800

    Created temp board, setting up msgarrvd

--- src/tictactoe.c
+++ src/tictactoe.c
@@ -1,9 +1,8 @@
-
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-
 #include "MQTTClient.h"
+#include "stdbool.h"
 
 #define ADDRESS     "tcp://broker.emqx.io:1883"
 #define CLIENTID    "broker.emqx.io"
@@ -11,11 +10,41 @@
 #define PAYLOAD     "Hello World!"
 #define QOS         1
 #define TIMEOUT     10000L
+volatile MQTTClient_deliveryToken deliveredtoken;
 
-char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
-void display();
+void displayBoard(void);
 int check();
+
+char square[10] = {'o','1','2','3','4','5','6','7','8','9'};
+
+void delivered(void *context, MQTTClient_deliveryToken dt)
+{
+    deliveredtoken = dt;
+}
+
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
+{
+    int i;
+    char* payloadptr;
+    printf("Message arrived\n");
+    printf("    topic: %s\n", topicName);
+    printf("    message: ");
+    payloadptr = message->payload;
+
+    if(){
+
+    }
+
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    return 1;
+}
+
+void connlost(void *context, char *cause)
+{
+    printf("\nConnection lost\n");
+    printf("    cause: %s\n", cause);
+}
 
 int main(int argc, char* argv[])
 {
@@ -23,37 +52,48 @@ int main(int argc, char* argv[])
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
     MQTTClient_message pubmsg = MQTTClient_message_initializer;
     MQTTClient_deliveryToken token;
+
     int rc;
 
-    MQTTClient_create(&client, ADDRESS, CLIENTID,
-        MQTTCLIENT_PERSISTENCE_NONE, NULL);
-  
-    // MQTT Connection parameters
+    MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+
     conn_opts.keepAliveInterval = 20;
     conn_opts.cleansession = 1;
 
-    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
+
+    if((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
     {
         printf("Failed to connect, return code %d\n", rc);
-        exit(-1);
+        exit(EXIT_FAILURE); //exit(-1);
     }
-  
-    // Publish message
+
+    //Publish Message
     pubmsg.payload = PAYLOAD;
     pubmsg.payloadlen = strlen(PAYLOAD);
     pubmsg.qos = QOS;
     pubmsg.retained = 0;
+
     MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
-    printf("Waiting for up to %d seconds for publication of %s\n"
-            "on topic %s for client with ClientID: %s\n",
-            (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
+
+    printf("Waiting for up to %d seconds for pubication of %s\n" "on toic %s for client with ClientID: %s\n", (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
     rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
-    printf("Message with delivery token %d delivered\n", token);
-  
-    // Disconnect
+    
+    //end connection
     MQTTClient_disconnect(client, 10000);
     MQTTClient_destroy(&client);
     return rc;
+}
+
+void displayBoard(void)
+{
+    printf("\n+-----------+\n");
+    printf("| %c | %c | %c |", square[1], square[2], square[3]);
+    printf("\n+-----------+\n");
+    printf("| %c | %c | %c |", square[4], square[5], square[6]);
+    printf("\n+-----------+\n");
+    printf("| %c | %c | %c |", square[7], square[8], square[9]);
+    printf("\n+-----------+\n");
 }
 
 //Check board if all equal
@@ -87,7 +127,7 @@ int check()
         square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] 
         != '7' && square[8] != '8' && square[9] != '9')
 
-        return 0;
+        return 0; // if tie
     else
-        return  - 1;
+        return -1;
 }
